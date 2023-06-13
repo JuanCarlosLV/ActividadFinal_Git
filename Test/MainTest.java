c89cc032cee5b7d973a3e3ebc2d6d8bf8f68f518
Juan Carlos LV <lopesjuancarlos216@gmail.com>
2023-06-13 00:47:58 -0600

    Cambios en rama Feature, se modifico metodo imprimirRango y Tests

--- Test/MainTest.java
+++ Test/MainTest.java
@@ -8,9 +8,9 @@ public class MainTest {
     @Test
     public void test1() {
         System.out.println("Test 1");
-        int primerNumero = 5;
+        int primerNumero = 1;
         int segundoNumero = 10;
-        String esperado = "6\n8\n";
+        String esperado = "3\n5\n7\n9\n";
         String actual = Main.imprimirRango(primerNumero, segundoNumero);
         assertEquals(esperado, actual);
     }
@@ -32,7 +32,7 @@ public class MainTest {
         System.out.println("Test 3");
         int primerNumero = -4;
         int segundoNumero = 4;
-        String esperado = "-2\n0\n2\n";
+        String esperado = "-3\n-1\n1\n3\n";
         String actual = Main.imprimirRango(primerNumero, segundoNumero);
         assertEquals(esperado, actual);
     }
@@ -56,14 +56,6 @@ public class MainTest {
         String actual = Main.imprimirRango(primerNumero, segundoNumero);
         assertEquals(esperado, actual);
     }
-    @Test
-    public void test6() {
-        System.out.println("Test 6");
-        int primerNumero = 1;
-        int segundoNumero = 10;
-        String esperado = "2\n4\n6\n8";
-        String actual = Main.imprimirRango(primerNumero, segundoNumero);
-        assertEquals(esperado, actual);
-    }
+ 
 
 }
